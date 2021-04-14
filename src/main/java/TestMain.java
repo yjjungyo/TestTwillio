84bc95fcefcc3873c1aa13bcadd08566b3a31d80
yjjungyo <yjjungyo@gmail.com>
2021-04-15 00:06:23 +0900

    2nd

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -12,14 +12,17 @@ public class TestMain {
     // Find your Account Sid and Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
     public static final String ACCOUNT_SID = "AC02170191774650c06810101ccbfe3733";
-    public static final String AUTH_TOKEN = "0cb10fe9266eb7db0fff5db36dea4889";
+    public static final String AUTH_TOKEN = "5d8ef421c8b24ce6892c18eb2a5cb5f6";
+
 
     public static void main(String[] args) {
+
+        Twilio.destroy();
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber("+821062350994"), //receiver
                 new com.twilio.type.PhoneNumber("+15868003710"),  //sender, twillio phone number
-                "Hi there!")
+                "Hi there!!!!")
                 .create();
 
         System.out.println(message.getSid());
