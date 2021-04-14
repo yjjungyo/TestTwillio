cfc97b7e6ddf611849fc95f2d60a5ac07d4daca5
yjjungyo <yjjungyo@gmail.com>
2021-04-15 00:17:56 +0900

    token changed

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,6 +1,6 @@
 
     //AC02170191774650c06810101ccbfe3733
-    //0cb10fe9266eb7db0fff5db36dea4889
+    //5d8ef421c8b24ce6892c18eb2a5cb5f6
     //+15868003710
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
@@ -21,7 +21,7 @@ public class TestMain {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber("+821062350994"), //receiver
-                new com.twilio.type.PhoneNumber("+15868003710"),  //sender, twillio phone number
+                new com.twilio.type.PhoneNumber("+15868003710"),  //sender, twilio phone number
                 "Hi there!!!!")
                 .create();
 
